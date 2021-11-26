a46a531ef60702387c28c2b4271fb6c69c75e32c
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-25 21:45:01 -0300

    feat: create header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../../assets/dogs.svg';
 import styles from './style.module.css';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>Login / Criar</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
