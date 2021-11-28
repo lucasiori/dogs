85691361523cb5a77a43a491768282d62dd938e4
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-28 18:03:49 -0300

    feat: create mobile menu

--- src/components/User/UserHeaderNav/index.js
+++ src/components/User/UserHeaderNav/index.js
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../../contexts/UserContext';
+import useMedia from '../../../hooks/useMedia';
 import { ReactComponent as MyPhotos } from '../../../assets/feed.svg';
 import { ReactComponent as Stats } from '../../../assets/stats.svg';
 import { ReactComponent as AddPhoto } from '../../../assets/add.svg';
@@ -8,31 +9,54 @@ import { ReactComponent as Logout } from '../../../assets/logout.svg';
 import styles from './style.module.css';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = useState(null);
   const { userLogout } = useContext(UserContext);
+  const mobile = useMedia('(max-width: 720px)');
+  const { pathname } = useLocation();
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/my-account" end>
-        <MyPhotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
+    <>
+      <button
+        aria-label="Menu"
+        className={`
+          ${styles.mobileButton}
+          ${mobile ? styles.mobileButtonVisible : ''} 
+          ${mobileMenu ? styles.mobileButtonActive : ''}
+        `}
+        onClick={() => setMobileMenu(!mobileMenu)}
+      />
 
-      <NavLink to="/my-account/stats">
-        <Stats />
-        {mobile && 'Estatísticas'}
-      </NavLink>
+      <nav 
+        className={
+          `${mobile ? styles.navMobile : styles.nav} ${mobileMenu ? styles.navMobileActive : ''}
+        `}
+      >
+        <NavLink to="/my-account" end>
+          <MyPhotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
 
-      <NavLink to="/my-account/post">
-        <AddPhoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
+        <NavLink to="/my-account/stats">
+          <Stats />
+          {mobile && 'Estatísticas'}
+        </NavLink>
 
-      <button onClick={userLogout}>
-        <Logout />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+        <NavLink to="/my-account/post">
+          <AddPhoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+
+        <button onClick={userLogout}>
+          <Logout />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 }
 
---
+++ src/hooks/useMedia.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const useMedia = (media) => {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+
+    window.addEventListener('resize', changeMatch);
+    changeMatch();
+
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    }
+  }, [media]);
+
+  return match;
+}
+
+export default useMedia;
