d3cc4414b51dde2a132fa7a68f01f0e10cda11a4
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-28 16:36:32 -0300

    feat: create functionality to create new user

--- src/api/index.js
+++ src/api/index.js
@@ -37,9 +37,23 @@ function USER_GET(token) {
   };
 }
 
+function USER_POST(body) {
+  return {
+    url: `${API_URL}/api/user`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }
+  };
+}
+
 export {
   API_URL,
   TOKEN_POST,
   TOKEN_VALIDATE_POST,
-  USER_GET
+  USER_GET,
+  USER_POST
 };
--- src/components/LoginCreate/index.js
+++ src/components/LoginCreate/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import styles from './style.module.css';
+import React, { useContext } from 'react';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../contexts/UserContext';
+import useForm from '../../hooks/useForm';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginCreate = () => {
-  return <div className={styles.loginCreate}>LoginCreate</div>
+  const { userLogin } = useContext(UserContext);
+
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+  
+    const response = await fetch(url, options);
+    
+    if (response.ok) {
+      userLogin(username.value, password.value);
+    }
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  )
 }
 
 export default LoginCreate;
